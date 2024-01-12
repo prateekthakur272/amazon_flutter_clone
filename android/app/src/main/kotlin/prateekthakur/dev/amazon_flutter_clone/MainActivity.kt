7dbd4963a9dfc32742e0a3dd6a0dc2b6aa08ff69
Prateek Thakur <prateekthakur272@gmail.com>
2024-01-12 14:33:27 +0530

    first commit

---
+++ android/app/src/main/kotlin/prateekthakur/dev/amazon_flutter_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package prateekthakur.dev.amazon_flutter_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Amazon',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Placeholder(),
+    );
+  }
+}

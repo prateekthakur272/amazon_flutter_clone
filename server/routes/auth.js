aa9efec4842059a1612c14486c2d04ed6376552c
Prateek Thakur <prateekthakur272@gmail.com>
2024-01-22 00:16:46 +0530

    adds error handling

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,14 +4,19 @@ const User = require('../models/user')
 const router = express.Router();
 
 router.post('/api/signup', async (req,res)=>{
-    const {name, email, password} = req.body;
-    const user = await User.findOne({email})
-    if (user){
-        return res.status(400).json({message: `User with email ${email} already exists.`})
+    try{
+        const {name, email, password} = req.body;
+        const user = await User.findOne({email})
+        if (user){
+            return res.status(400).json({message: `User with email ${email} already exists.`})
+        }
+        let newUser = new User({email, name, password})
+        newUser = await newUser.save()
+        res.status(201).json({email, name, id: newUser.id})
+    }catch(e) {
+        console.error(e);
+        res.status(500).json({error: 'some error occurred'})
     }
-    let newUser = new User({email, name, password})
-    newUser = await newUser.save()
-    res.status(201).json({email, name, id: newUser.id})
 })
 
 module.exports = router

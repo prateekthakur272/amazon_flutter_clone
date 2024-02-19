9aac001e9ad9d3fabe8df50dac5dd9633fc85585
Prateek Thakur <prateekthakur272@gmail.com>
2024-02-19 13:10:01 +0530

    adds admin middleware

---
+++ server/middlewares/admin.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+const {passwordKey} = require('../config');
+const { User } = require('../models/user');
+const admin = async (req, res, next) => {
+    try{
+        const token = req.header('x-auth-token');
+        if(!token){
+            return res.status(401).json({message: '[x-auth-token] required'})
+        }
+        const verified = jwt.verify(token, passwordKey);
+        if(!verified){
+            return res.status(401).json({message: 'invalid token'})
+        }
+        const user = await User.findById(verified.id)
+        if(user.type != 'admin'){
+            return res.status(401).json({message: 'you are not an admin'})
+        }
+        req.user = verified.id;
+        req.token = token;
+        next();
+    }catch(e){
+        return res.status(500).json({error: e.message})
+    }
+}
+
+module.exports = {admin}
---
+++ server/routes/admin.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const router = express.Router()
+const {admin} = require('../middlewares/admin')
+
+
+
+module.exports = router;

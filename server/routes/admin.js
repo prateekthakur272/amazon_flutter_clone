fd5d27b6550a16207c74e64fe8caed776629bdee
Prateek Thakur <prateekthakur272@gmail.com>
2024-02-21 22:11:06 +0530

    project update

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,8 +1,18 @@
 const express = require('express');
 
 const router = express.Router()
-const {admin} = require('../middlewares/admin')
+const {admin} = require('../middlewares/admin');
+const Product = require('../models/product');
 
-
+router.post('/admin/add-product', admin, async (req, res) => {
+    try {
+        const {name, description, images, quantity, price, category} = req.body;
+        let product = new Product({name, description, images, quantity, price, category});
+        product = await product.save();
+        return res.json(product);
+    } catch (e) {
+        return res.status(500).json({error : e.message})
+    }
+})
 
 module.exports = router;
